ddad5e41d09656d5979a831d64c57f8677fce046
Bushra Yousaf <smileforbushi@gmail.com>
2025-05-23 23:27:31 +0500

    Fix DELETE request CORS

--- server/server.js
+++ server/server.js
@@ -22,7 +22,13 @@ const corsOptions = {
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Must include DELETE
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "Cache-Control",
+    "Expires",
+    "Pragma",
+  ],
 };
 
 //router.delete("/api/post/deletepost/:id", authMiddleware, deletepost);
