55fa9f5d14775a77800485356b464602e9221192
Bushra Yousaf <smileforbushi@gmail.com>
2025-05-24 11:43:25 +0500

    Fixed CORS configuration

--- server/models/Connection.js
+++ server/models/Connection.js
@@ -3,8 +3,20 @@ require("dotenv").config();
 
 mongoose
   .connect(process.env.MONGO_URI, {
-    serverSelectionTimeoutMS: 30000, // 30 seconds timeout
-    socketTimeoutMS: 45000, // 45 seconds socket timeout
+    serverSelectionTimeoutMS: 30000,
+    socketTimeoutMS: 45000,
   })
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.error("MongoDB Connection Error:", err));
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose connected to DB");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error("Mongoose connection error:", err);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongoose disconnected");
+});
